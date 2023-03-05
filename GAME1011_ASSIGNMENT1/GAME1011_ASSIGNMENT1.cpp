a4a4efe78b8256beb9015642003dbfddd0dc05e7
elentiyafireheart <samantha-emeline@hotmail.com>
2023-03-05 12:46:17 -0700

    Enemy created at random

--- GAME1011_ASSIGNMENT1/GAME1011_ASSIGNMENT1.cpp
+++ GAME1011_ASSIGNMENT1/GAME1011_ASSIGNMENT1.cpp
@@ -9,6 +9,7 @@
 #include "Wizard.h"
 
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
@@ -17,7 +18,10 @@ int main()
 
 	int playerChoice;
 	int battleReady;
+	int randomEnemy;
 	string playerName;
+
+	srand(time(NULL));
 
 	cout << "         |---------- Welcome to the Simulator ----------|" << endl;
 	cout << "To continue your quest, please select either Wizard (1) or Knight (2)." << endl;
@@ -31,48 +35,68 @@ int main()
 		p1->SetName(playerName);
 		p1->SetHealth(200);
 
-		cout << "\nHere is your current player information:";
+		cout << "\nHere is your current player information:" << endl;
 		cout << "Class: Wizard" << endl;
 		cout << "Name: " << p1->GetName() << endl;
 		cout << "Health: " << p1->GetHealth() << endl;
 
-		cout << "\nAre you ready to witness a battle? Yes (1) or No (2)";
+		cout << "\nAre you ready to witness a battle? Yes (1) or No (2)" << endl;
 		cin >> battleReady;
 
 		if (battleReady == 1)
 		{
+			randomEnemy = rand() % 50 + 1;
 
+			if (randomEnemy > 1 && randomEnemy < 50)
+			{
+				Orc* e1 = new Orc;
+			
+			}
+			else if (randomEnemy > 51 && randomEnemy < 100)
+			{
+				Undead* e2 = new Undead;
+
+			}
 		}
 		else if (battleReady == 2)
 		{
-
+			cout << "You ran from battle..." << endl;
 		}
 
 	}
 	else if(playerChoice == 2)
 	{
 		Knight* p2 = new Knight;
-		cout << "knight";
 		cout << "What would you like to name your character?" << endl;
 		cin >> playerName;
 		p2->SetName(playerName);
 		p2->SetHealth(250);
 
-		cout << "\nHere is your current player information:";
-		cout << "Class: Wizard" << endl;
+		cout << "\nHere is your current player information:" << endl;
+		cout << "Class: Knight" << endl;
 		cout << "Name: " << p2->GetName() << endl;
 		cout << "Health: " << p2->GetHealth() << endl;
 
-		cout << "\nAre you ready to witness a battle? Yes (1) or No (2)";
+		cout << "\nAre you ready to witness a battle? Yes (1) or No (2):" << endl;
 		cin >> battleReady;
 
 		if (battleReady ==1)
 		{
-			
+			randomEnemy = rand() % 100 + 1;
+
+			if (randomEnemy > 1 && randomEnemy < 50)
+			{
+				Orc* e1 = new Orc;
+
+			}
+			else if (randomEnemy > 51 && randomEnemy < 100)
+			{
+				Undead* e2 = new Undead;
+			}
 		}
 		else if (battleReady == 2)
 		{
-			
+			cout << "You ran from battle..." << endl;
 		}
 	}
 }
